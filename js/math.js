d56d6f502d46aecbc73e036b8642cacb26cfee12
Chris <chrisdniels12@gmail.com>
2024-05-22 12:21:05 -0700

    Update math.js
    finish part 2, onto part 3

--- js/math.js
+++ js/math.js
@@ -1,9 +1,23 @@
 //STEP 1
+// let num = parseInt(prompt("Enter a number"))
+// console.log(Math.abs(num))
 
 //STEP 2
-
+// let deci = parseFloat(prompt("Enter a floating point number"))
+// console.log(Math.ceil(deci))
 //STEP 3
-
+// let deci = parseFloat(prompt("Enter a floating point number"))
+// console.log(Math.floor(deci))
 //STEP 4
+// let input = prompt("Please enter 5 numbers separated by commas (e.g., 1,2,3,4,5):");
+// let numbers = input.split(",").map(Number);
+
+// let largest = Math.max(...numbers);
+// let smallest = Math.min(...numbers);
+
+// console.log("Largest number:", largest);
+// console.log("Smallest number:", smallest);
 
 //STEP 5
+// let num = parseInt(prompt("Enter a number"))
+// console.log(Math.sqrt(num))
